7a4094dea2565f5976bfb6cb6c66732a8e43d024
nosrac77 <carson.newton@outlook.com>
2018-09-12 19:06:42 -0700

    modified a line in update_git.py that created more readability when prompting user for their github commit message.

--- update_git.py
+++ update_git.py
@@ -16,7 +16,7 @@ def run_acp_config_files(branch_name):
     call(['sudo', 'cp', '-r', path_to_original_config_dir, path_to_project_dir])
     call(['sudo', 'mv', path_to_project_dir + original_dir, path_to_project_dir + '/homeassistant_config_backup'])
     call(['sudo', 'git', 'add', '.'])
-    commit_message = input('Please enter a Github commit message')
+    commit_message = input('Please enter a Github commit message: ')
     call(['sudo', 'git', 'commit', '-m', commit_message])
     call(['sudo', 'git', 'push', 'origin', branch_name])
 
