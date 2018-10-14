6c8e3361390a5fcdc1d73331461274a2806b6b62
nosrac77 <carson.newton@outlook.com>
2018-10-14 15:58:48 -0700

    code from body_cam.py and motion_sensor.py has been successfully combined to produce a motion detecting video monitoring device. The module body_cam.py has the combined code, motion_sensor.py is now obsolete but is being kept as a reference for setting up a PIR sensor.

--- src/body_cam_files/body_cam.py
+++ src/body_cam_files/body_cam.py
@@ -1,13 +1,27 @@
 """Module which holds PiCamera configurations."""
 
+import RPi.GPIO as GPIO
 from picamera import PiCamera
 from time import sleep
 
-camera = PiCamera()
+pir_sensor = 11
+current_state = 0
 
-camera.start_preview()
-camera.start_recording('/home/pi/Desktop/rpi_recording_test.h264')
-sleep(5)
-camera.stop_recording()
-camera.stop_preview()
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(pir_sensor, GPIO.IN)
+
+try:
+    while True:
+        sleep(0.1)
+        current_state = GPIO.input(pir_sensor)
+        if current_state == 1:
+            with PiCamera() as camera:
+                camera.start_preview()
+                camera.start_recording('/home/pi/Desktop/motion_detector_test.h264')
+                sleep(10)
+                camera.stop_recording()
+                camera.stop_preview()
+except(KeyboardInterrupt):
+    print('Monitoring process: terminated.')
+    GPIO.cleanup()
 
