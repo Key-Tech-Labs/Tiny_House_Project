432c8498ee95925d118f65ac3dd1ff3e9fe8ee80
nosrac77 <carson.newton@outlook.com>
2018-10-14 17:31:49 -0700

    added text and timestamp to recorded video in body_cam.py

--- src/body_cam_files/body_cam.py
+++ src/body_cam_files/body_cam.py
@@ -2,7 +2,7 @@
 
 import RPi.GPIO as GPIO
 from picamera import PiCamera
-from time import sleep
+from time import sleep, asctime
 
 pir_sensor = 11
 current_state = 0
@@ -17,6 +17,7 @@ try:
         if current_state == 1:
             with PiCamera() as camera:
                 camera.start_preview()
+                camera.annotate_text = 'Recording triggered by motion: {}'.format(asctime())
                 camera.start_recording('/home/pi/Desktop/motion_detector_test.h264')
                 sleep(10)
                 camera.stop_recording()
